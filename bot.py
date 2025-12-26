3248e890f8f20b0a025db4f7deae9801e0f62b49
Wirlian Alfathin <106651413+Black1ssl@users.noreply.github.com>
2025-12-27 03:39:35 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,41 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
+
+BOT_TOKEN = "8571822830:AAH0PRPvBEDEzOa3AjZbBFSMGnsM9UDs3uQ"
+
+TARGET_CHANNEL_ID = -1003595038397 # ID CHANNEL
+
+REQUIRED_TAG = "#POST"
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg = update.message
+
+   
+
+    text = msg.text or msg.caption or ""
+
+    # Validasi tag
+    if REQUIRED_TAG not in text:
+        await msg.reply_text(
+            f"❌ Post ditolak.\nWajib menyertakan tag {REQUIRED_TAG}"
+        )
+        return
+
+    # Kirim ke channel
+    if msg.photo:
+        await context.bot.send_photo(
+            chat_id=TARGET_CHANNEL_ID,
+            photo=msg.photo[-1].file_id,
+            caption=msg.caption
+        )
+    else:
+        await context.bot.send_message(
+            chat_id=TARGET_CHANNEL_ID,
+            text=msg.text
+        )
+
+    await msg.reply_text("✅ Post berhasil dikirim ke channel.")
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(MessageHandler(filters.ALL, handle_message))
+app.run_polling()
